ad0bbccb7726b4f36e9e3b227f619bfcb78104ec
ShubratoDn <86429182+ShubratoDn@users.noreply.github.com>
2022-05-15 14:22:09 +0600

    uploading all file

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {navSticky()};
+        
+var navbar = document.getElementById("navbar");
+var container = document.getElementById("container");
+var sticky = navbar.offsetTop;
+
+function navSticky() {
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+        container.style.marginTop = sticky+"px";
+    }else{
+        navbar.classList.remove("sticky");
+        container.style.marginTop = "0";
+    }
+}
