a25a39cbe387046db9c1d11cbf3a1ba4d72c711e
ShubratoDn <86429182+ShubratoDn@users.noreply.github.com>
2022-05-22 00:58:07 +0600

    login and content detail page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-window.onscroll = function() {navSticky()};
-        
+window.onscroll = function(){navSticky(); navHorLogo()};
+
 var navbar = document.getElementById("navbar");
 var container = document.getElementById("container");
 // var sticky = navbar.offsetTop;
 var sticky = navbar.offsetHeight;
+var navHorMenu = document.querySelector(".nav-hor-menu");
+var navbarNav = document.querySelector(".navbar-nav");
+var navHorMenuLogo = document.querySelector(".nav-hor-menu-logo");
+
 
 function navSticky() {
+    // pageYoffset and scrollY both are equal
     if(window.pageYOffset >= sticky){
         navbar.classList.add("sticky");
         container.style.marginTop = sticky+"px";
@@ -13,4 +18,20 @@ function navSticky() {
         navbar.classList.remove("sticky");
         container.style.marginTop = "0";
     }
+}
+
+// correcting navbar position
+var prevPos =  window.scrollY;
+function navHorLogo(){
+    var currentPos = window.scrollY;
+    if(prevPos > currentPos){
+        // navHorMenu.classList.add('active');
+        navbarNav.style.transform = "translateY(00px)";
+        navHorMenuLogo.style.transform = "translateY(100px)";        
+    }else{
+        // navHorMenu.classList.remove('active')
+        navbarNav.style.transform = "translateY(-100px)";
+        navHorMenuLogo.style.transform = "translateY(00px)";
+    }
+    prevPos = currentPos;
 }
